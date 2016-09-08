73b8a2eb2ce99d69a677ea6bab45c7f66d8d5190
SyncMr <talk2pragadheesh@gmail.com>
2016-09-07 18:21:20 -0700

    LRU

--- OnlineLinks/LRUCache.cpp
+++ OnlineLinks/LRUCache.cpp
@@ -1,29 +1,41 @@
 #include <list>
 #include <unordered_map>
+#include <iostream>
+using namespace std;
 
-template <typename T>
 class LRUCache
 {
-public:
-    LRUCache(int capacity)
-    {
-        
-    }
-    
-    T get(int key)
-    {
+private:
+    int _capacity;
+    list<int> _lruList;
+    unordered_map<int, list<int>* > _lruMap;
 
-        
-    }
-    
-    void set(int key, T value)
-    {
-        
-    }
+public:
+    LRUCache(int capacity);
+    int get(int key);
+    void set(int key, int value);
 };
+
+LRUCache::LRUCache(int capacity) :
+          _capacity(capacity),
+          _lruList(capacity)
+{ }
+
+int
+LRUCache::get(int key)
+{
+    return 0;
+}
+
+void
+LRUCache::set(int key, int value)
+{
+    
+}
 
 int main()
 {
+    LRUCache 
     std::cout << "\n";
     return 0;
 }
