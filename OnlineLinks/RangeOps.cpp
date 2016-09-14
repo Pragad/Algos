d7f81cffb16ecba40d29facdf6faa4adf8ce56ea
SyncMr <talk2pragadheesh@gmail.com>
2016-09-14 10:18:57 -0700

    Range Ops

--- OnlineLinks/RangeOps.cpp
+++ OnlineLinks/RangeOps.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <bitset>
 
 class RangeOps
 {
@@ -42,35 +43,153 @@ private:
     std::unordered_map<signed long long, uint64_t> _rangeMap;
 
 public:
+    bool isRangeValid(int stRng, int enRng);
     bool addRange(int stRng, int enRng);
     bool queryRange(int stRng, int enRng);
     bool deleteRange(int stRng, int enRng);
+    void printRanges();
 };
+
+bool 
+RangeOps::isRangeValid(int stRng, int enRng)
+{
+    // return (stRng <= enRng);
+    return true;
+}
 
 bool
 RangeOps::addRange(int stRng, int enRng)
 {
-    Range ob(stRng, enRng);
-    auto itr = _rangeMap.find(ob.getRangeKey());
 
-    if (itr == _rangeMap.end())
+    if (isRangeValid(stRng, enRng))
     {
+        Range ob(stRng, enRng);
+
+        // Check if the Range is present already
+        // If the Range is already present then return false to indicate the the element
+        // did not get added
+        auto itr = _rangeMap.find(ob.getRangeKey());
+        if (itr != _rangeMap.end())
+        {
+            return false;
+        }
+
         _rangeList.push_back(ob);
+        _rangeMap[ob.getRangeKey()] = _rangeList.size() - 1;
+        return true;
     }
+    else
+    {
+        return false;
+    }
+}
+
+bool
+RangeOps::queryRange(int stRng, int enRng)
+{
+    if (isRangeValid(stRng, enRng))
+    {
+        Range ob(stRng, enRng);
+
+        // Check if the Range is present already
+        auto itr = _rangeMap.find(ob.getRangeKey());
+        if (itr != _rangeMap.end())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool
+RangeOps::deleteRange(int stRng, int enRng)
+{
+    if (isRangeValid(stRng, enRng))
+    {
+        Range ob(stRng, enRng);
+
+        // If the Range is not present then return false to indicate the the element
+        // is not present in our list
+        auto itr = _rangeMap.find(ob.getRangeKey());
+        if (itr == _rangeMap.end())
+        {
+            return false;
+        }
+
+        // Store the last element in a temp variable
+        Range tmp = _rangeList[_rangeList.size() - 1];
+
+        // Push the last element to the position where element should be deleted
+        _rangeList[itr->second] = tmp;
+        _rangeMap[tmp.getRangeKey()] = itr->second;
+
+        // Erase the last element from the vector
+        _rangeList.pop_back();
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void
+RangeOps::printRanges()
+{
+    std::cout << "Ranges List" << "\n";
+    for (auto item : _rangeList)
+    {
+        std::cout << item._startRange << " : " << item._endRange << " : " << item._rangeKey << "\n";
+        std::bitset<64> s(item._startRange);
+        std::bitset<64> e(item._endRange);
+        std::bitset<64> k(item._rangeKey);
+        std::cout << s << " : " << e << " : " << k << "\n";
+    }
+    std::cout << "\n\n";
 }
 
 int main()
 {
-    int a = 5;
-    int b = 7;
-    signed long long c = a;
+    int a1 = 5;
+    int b1 = 10;
+
+    int a2 = -5;
+    int b2 = -7;
+
+    int a3 = 1;
+    int b3 = 10;
+
+    int a4 = -15;
+    int b4 = -20;
+
+    int a5 = 10;
+    int b5 = 30;
 
     RangeOps ob;
-    ob.addRange(a, b);
+    std::cout << "AR 1: " << ob.addRange(a1, b1) << "\n";
+    std::cout << "AR 2: " << ob.addRange(a2, b2) << "\n";
+    std::cout << "AR 3: " << ob.addRange(a3, b3) << "\n";
+    ob.printRanges();
 
-    std::cout << sizeof(a) << "\n";
-    std::cout << sizeof(b) << "\n";
-    std::cout << sizeof(c) << "\n";
-    std::cout << "\n";
+    std::cout << "QR 1: " << ob.queryRange(a1, b1) << "\n";
+    std::cout << "QR 2: " << ob.queryRange(a2, b1) << "\n";
+    std::cout << "QR 3: " << ob.queryRange(a2, b2) << "\n";
+
+    std::cout << "DR 1: " << ob.deleteRange(a1, b2) << "\n";
+    std::cout << "DR 2: " << ob.deleteRange(a1, b1) << "\n";
+    std::cout << "DR 3: " << ob.deleteRange(a2, b2) << "\n";
+    ob.printRanges();
+
+    std::cout << "AR 4: " << ob.addRange(a2, b2) << "\n";
+    std::cout << "AR 5: " << ob.addRange(a4, b4) << "\n";
+    std::cout << "AR 6: " << ob.addRange(a5, b5) << "\n";
+    ob.printRanges();
+
+    std::cout << "DR 4: " << ob.deleteRange(a1, b1) << "\n";
+    std::cout << "DR 5: " << ob.deleteRange(a2, b2) << "\n";
+    ob.printRanges();
+
     return 0;
 }
