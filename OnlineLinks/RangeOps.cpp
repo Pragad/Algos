34a125c46a87273b962ab9159a28acff254f812f
SyncMr <talk2pragadheesh@gmail.com>
2016-09-14 00:43:16 -0700

    Range Ops

--- OnlineLinks/RangeOps.cpp
+++ OnlineLinks/RangeOps.cpp
@@ -17,9 +17,22 @@ private:
         Range(int stRng, int enRng) : _startRange(stRng),
                                       _endRange(enRng)
         {
+            // Mask is used to clear the upper four bits in a Signed Int
+            // Say Start Range  = -5 (1111 1011)
+            //     End Range    = -7 (1111 1001)
+            //     Key Should be= 1011 1001
+            //
+            // 1. Copy startRange to Key
+            // 2. Do a bitwise & to clear the upper four bits
+            // 3. Do a shift to accomodate space for EndRange
+            // 4. EndRange Bitwise & MASK will clear the upper four bits
+            // 5. Do an XOR of above with rangekey
+            int MASK = 0x0f;
             _rangeKey = stRng;
+            _rangeKey = _rangeKey & MASK;
+
             _rangeKey <<= sizeof(int);
-            _rangeKey ^= enRng;
+            _rangeKey ^= (enRng & MASK);
         }
 
         signed long long getRangeKey() { return _rangeKey; }
@@ -34,13 +47,26 @@ public:
     bool deleteRange(int stRng, int enRng);
 };
 
+bool
+RangeOps::addRange(int stRng, int enRng)
+{
+    Range ob(stRng, enRng);
+    auto itr = _rangeMap.find(ob.getRangeKey());
+
+    if (itr == _rangeMap.end())
+    {
+        _rangeList.push_back(ob);
+    }
+}
+
 int main()
 {
     int a = 5;
     int b = 7;
     signed long long c = a;
-    c <<= sizeof(int);
-    c ^= b;
+
+    RangeOps ob;
+    ob.addRange(a, b);
 
     std::cout << sizeof(a) << "\n";
     std::cout << sizeof(b) << "\n";
