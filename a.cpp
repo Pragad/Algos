f9e921428aaca7256ea03bed79902fc1618cbd34
SyncMr <talk2pragadheesh@gmail.com>
2016-12-20 10:14:42 -0800

    File system project notes

--- a.cpp
+++ a.cpp
@@ -14,6 +14,46 @@
 
 using namespace std;
 
+struct dirEntry
+{
+    uint8_t     dirType;                // Type of the sub directory.
+    int8_t      fileName[9];            // Name of the sub directory.
+    uint16_t    lastBlockUsedBytes;     // This will be 512 for a directory.
+                                        // For a file it will range from 5 - 512 OR 0 - 504
+                                        // For a file it will tell where the last entry of
+                                        // the file is stored.
+    uint32_t    actualBlockNumber;      // This will range between 1 to 100 to show the
+                                        // block number that has the sub directory.
+};
+
+struct directory
+{
+    int32_t    prevBlock;
+    int32_t    nextBlock;
+    int32_t    firstUnusedBlock;
+    uint32_t   fillerChar;
+    dirEntry   dirEntries[31];
+};
+
+struct file
+{
+    int32_t    prevBlock;
+    int32_t    nextBlock;
+    uint8_t    fileData[504];
+};
+
+union block
+{
+    directory   blockDir;
+    file        blockFile;
+};
+
+struct disk
+{
+    block blocks[100];
+    //directory blocks[100];
+} myDisk;
+
 int main()
 {
     cout << sizeof(int) << endl;
@@ -28,6 +68,12 @@ int main()
     {
         cout << "-1 is False" << endl;
     }
+
+    cout << "Struct Size" << endl;
+    cout << sizeof(myDisk) << endl;
+    cout << sizeof(file) << endl;
+    cout << sizeof(directory) << endl;
+
     cout << endl;
     return 0;
 }
